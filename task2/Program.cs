58fb3b478718cb40f874f6c0c9c04b56c3f11535
DARYA <darina.instaface@gmail.com>
2022-10-02 17:38:11 +0300

    task2_2

--- task2/Program.cs
+++ task2/Program.cs
@@ -6,4 +6,14 @@ int numberA = int.Parse(Console.ReadLine()!);
 int numberB = int.Parse(Console.ReadLine()!);
 int numberC = int.Parse(Console.ReadLine()!);
 
+int max = numberA;
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine($"Максимальное из введённых чисел равно {max}");
 
